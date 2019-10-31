8fd5154bc75665690c0d406bd0ef7bb73934cc2d
joey abouharb <joeyabouharb@localhost.localdomain>
2019-10-31 21:02:02 +1100

    removed compression

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const gzip = require('express-static-gzip');
 const path = require('path');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost:';
 const app = express();
 
-
-app.use('/', gzip(path.join('./dist'), {
-    enableBrotli: true,
-    orderPreference: ['br', 'gzip'],
-}));
+app.use(express.static('dist'))
 
 app.listen(port, (err) => {
         if (err) {
