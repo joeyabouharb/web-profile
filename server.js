6407baf35cb029bbf5876ad91f19ad961432039c
joey abouharb <joeyabouharb@localhost.localdomain>
2019-12-03 10:30:20 +1100

    added csp

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const helmet = require('helmet')
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost:';
 const app = express();
 
 app.use(express.static('dist'))
+app.use(helmet());
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com"],
+        scriptSrc: ["'self'", "https://cdnjs.cloudflare.com/"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+        imgSrc: ["'self'", "https://cdn.jsdelivr.net"]
+    }
+}))
 
 app.listen(port, (err) => {
         if (err) {
